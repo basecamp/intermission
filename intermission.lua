b18c29a67d5a293f809c4739377e40ff98408520
Taylor (themcgruff) <taylor@37signals.com>
2013-02-02 17:00:12 -0500

    Fix some bugs and adjust some things.

--- intermission.lua
+++ intermission.lua
@@ -1,42 +1,47 @@
 --Copyright 2012 37signals / Taylor Weibley
-local sleep_time = ngx.var.intermission_interval;
-local max_time = ngx.var.intermission_max_time;
+local sleep_time = tonumber(ngx.var.intermission_interval);
+local max_time = tonumber(ngx.var.intermission_max_time);
+local pausedreqs = ngx.shared.pausedreqs;
 
-  if pausedreqs:get('enabled') then
-    ngx.log(ngx.ERR, "Entering pause mode: " .. x_pause_id)
+if pausedreqs:get('enabled') then
+  ngx.log(ngx.ERR, "Entering pause mode: " .. x_pause_id)
   
-    --If this fails it already exists, no big deal.
+  --Auto inc counter for each paused request
+  if pausedreqs:get('id') == nil then
     pausedreqs:set('id', 0)
+  end
+  
+  --Increment the id
+  local id = pausedreqs:incr('id', 1)
+  ngx.log(ngx.ERR, 'Pause id:' .. id)
 
-    --Increment the id
-    local id = pausedreqs:incr('id', 1)
-    ngx.log(ngx.ERR, 'Pause id:' .. id)
+  --Add to our queue of paused requests
+  local succ, err = pausedreqs:add('paused:' .. id, 1)
 
-    local succ, err = pausedreqs:add('paused:' .. id, 1)
+  if succ then
     ngx.log(ngx.ERR, 'Pause id added ' .. tostring(succ))
 
-    local count = 0;
+    local wait_time = 0;
 
-    if succ then
-      ngx.log(ngx.ERR, 'Pause id successfully added.')
-
-      repeat
-        if pausedreqs:get('paused:' .. id + 1) == 0 or (pausedreqs:get('paused:' .. id + 1) == nil and id ~= 1) then
-            pausedreqs:set('paused:' .. id, 0)
-            pausedreqs:delete('paused:' .. id + 1)
-            return
-        else
-          ngx.sleep(sleep_time)
-          count = count + 0.05
-          ngx.log(ngx.ERR, "Count is: " .. count)
-        end
-      until count > max_time
-    else
-      ngx.log(ngx.ERR, 'Failed to add pause id.')
-    end
-
+    repeat
+      --First check the global state then check if the request ahead of us is still here
+      if pausedreqs:get('enabled') == nil and pausedreqs:get('paused:' .. id - 1) == nil then
+        ngx.log(ngx.ERR, "Unpause id: " .. id .. " after " .. wait_time .. " seconds")
+        pausedreqs:delete('paused:' .. id)
+        return
+      else
+        ngx.sleep(sleep_time)
+        wait_time = wait_time + sleep_time
+        --Warning will be *very* noisy with a small sleep_time
+        ngx.log(ngx.DEBUG, 'Pause id:' .. id .. ' waiting for ' .. wait_time .. ' seconds total')
+      end
+    until wait_time > max_time
   else
-    ngx.log(ngx.ERR, 'Pause not enabled')
-  
-    return
+    ngx.log(ngx.ERR, 'Failed to add pause id.')
   end
+
+else
+  --Self explanatory
+  ngx.log(ngx.ERR, 'Pause not enabled')
+  return
+end
